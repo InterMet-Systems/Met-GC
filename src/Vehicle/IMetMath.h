260d24f23dfd0c8100ab583b1b440e748ae149bd
Travis-Dunn <t_a_dunn@proton.me>
2025-03-03 15:50:04 -0600

    bulletproof math helpers

--- src/Vehicle/IMetMath.h
+++ src/Vehicle/IMetMath.h
@@ -1,18 +1,23 @@
 #ifndef IMETMATH_H
 #define IMETMATH_H
 
-#include <algorithm>
-
 namespace IMetMath {
 
 inline constexpr double pi = 3.14159265358979323846;
+inline constexpr double absoluteZeroK = 0.;
+inline constexpr double absoluteZeroC = -273.15;
+inline constexpr double absoluteZeroF = -459.67;
+inline constexpr double absoluteZeroR = 0.;
 
 enum class Result{
     _SUCCESS,
     _FAILURE,
-    _NAN,
-    _INF,
-    _EXCEEDS_RANGE,
+    _INPUT_NAN,
+    _INPUT_INF,
+    _OUTPUT_NAN,
+    _OUTPUT_INF,
+    _INPUT_EXCEEDS_RANGE,
+    _OUTPUT_EXCEEDS_RANGE,
 };
 
 struct SResult{
@@ -20,27 +25,52 @@ struct SResult{
     double value;
 };
 
-constexpr double toRadians(double degrees) {
-    return std::clamp((degrees * (pi / 180.)), 0., 2. * pi);
+constexpr SResult CelsiusToKelvin(const double& celsius) {
+    if (std::isnan(celsius))                return  { Result::_INPUT_NAN, 0.0 };
+    if (std::isinf(celsius))                return  { Result::_INPUT_INF, 0.0 };
+    const double kelvin = celsius + 273.15;
+    if (std::isnan(kelvin))                 return  { Result::_OUTPUT_NAN, 0.0 };
+    if (std::isinf(kelvin))                 return  { Result::_OUTPUT_INF, 0.0 };
+    if (celsius < absoluteZeroC)            return  { Result::_INPUT_EXCEEDS_RANGE, kelvin };
+    if (kelvin < absoluteZeroK)             return  { Result::_OUTPUT_EXCEEDS_RANGE, kelvin };
+
+    return { Result::_SUCCESS, kelvin };
 }
 
-constexpr double toDegrees(double radians) {
-    return std::clamp((radians * (180. / pi)), 0., 360.);
+constexpr SResult KelvinToCelsius(const double& kelvin) {
+    if (std::isnan(kelvin))                 return  { Result::_INPUT_NAN, 0.0 };
+    if (std::isinf(kelvin))                 return  { Result::_INPUT_INF, 0.0 };
+    const double celsius = kelvin - 273.15;
+    if (std::isnan(celsius))                return  { Result::_OUTPUT_NAN, 0.0 };
+    if (std::isinf(celsius))                return  { Result::_OUTPUT_INF, 0.0 };
+    if (kelvin < absoluteZeroK)             return  { Result::_INPUT_EXCEEDS_RANGE, celsius };
+    if (celsius < absoluteZeroC)            return  { Result::_OUTPUT_EXCEEDS_RANGE, celsius };
+
+    return { Result::_SUCCESS, celsius };
 }
 
-constexpr double toCelsius(double kelvin) {
-    /* What if kelvin is negative? That's clearly a garbage value, but in some math context perhaps one wants to have that?
-     * Should we address out of bounds values here, or in the calling context? Same kind of applies to the above functions. */
-    return kelvin - 273.15;
-}
-
-/* new approach */
 constexpr SResult DegreesToRadians(const double& degrees){
-    double r = degrees * (pi / 180.);
-    if (std::isnan(r)) return { Result::_NAN, r };
-    if (std::isinf(r)) return { Result::_INF, r };
-    if (r < 0. || r > 2 * pi) return { Result::_EXCEEDS_RANGE, r };
-    return { Result::_SUCCESS, r};
+    if (std::isnan(degrees))                return  { Result::_INPUT_NAN, 0.0 };
+    if (std::isinf(degrees))                return  { Result::_INPUT_INF, 0.0 };
+    const double radians = degrees * (pi / 180.);
+    if (std::isnan(radians))                return  { Result::_OUTPUT_NAN, 0.0 };
+    if (std::isinf(radians))                return  { Result::_OUTPUT_INF, 0.0 };
+    if (degrees < 0. || degrees > 360.)     return  { Result::_INPUT_EXCEEDS_RANGE, radians };
+    if (radians < 0. || radians > 2 * pi)   return  { Result::_OUTPUT_EXCEEDS_RANGE, radians };
+
+    return { Result::_SUCCESS, radians};
+}
+
+constexpr SResult RadiansToDegrees(const double& radians){
+    if (std::isnan(radians))                return  { Result::_INPUT_NAN, 0.0 };
+    if (std::isinf(radians))                return  { Result::_INPUT_INF, 0.0 };
+    const double degrees = radians * (180. / pi);
+    if (std::isnan(degrees))                return  { Result::_OUTPUT_NAN, 0.0 };
+    if (std::isinf(degrees))                return  { Result::_OUTPUT_INF, 0.0 };
+    if (radians < 0. || radians > 2 * pi)   return  { Result::_INPUT_EXCEEDS_RANGE, degrees };
+    if (degrees < 0. || degrees > 360.)     return  { Result::_OUTPUT_EXCEEDS_RANGE, degrees };
+
+    return { Result::_SUCCESS, degrees };
 }
 
 }
